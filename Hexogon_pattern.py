9fd2c9ef6bcaa8d23ad80cff956cbbc01fc0d8e0
Athul120 <87671514+Athul120@users.noreply.github.com>
2024-07-13 09:33:09 +0530

    Update Hexogon_pattern.py

--- Hexogon_pattern.py
+++ Hexogon_pattern.py
@@ -1,22 +1,22 @@
 def print_hexagon_grid(rows, columns):
     def print_hexagon_top():
-        for _ in range(columns):
+        for i in range(columns):
             print("  ___  ", end="")
         print()
 
     def print_hexagon_middle():
-        for _ in range(columns):
+        for i in range(columns):
             print(" /   \\ ", end="")
         print()
-        for _ in range(columns):
+        for i in range(columns):
             print("/     \\", end="")
         print()
 
     def print_hexagon_bottom():
-        for _ in range(columns):
+        for i in range(columns):
             print("\\     /", end="")
         print()
-        for _ in range(columns):
+        for i in range(columns):
             print(" \\___/ ", end="")
         print()
 
